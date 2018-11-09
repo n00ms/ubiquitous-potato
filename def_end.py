a77c1def0ef0ad91908592c4456788c4ad335e42
n00ms <44762430+n00ms@users.noreply.github.com>
2018-11-09 11:01:42 +0800

    Update def_end.py

--- def_end.py
+++ def_end.py
@@ -7,14 +7,15 @@ def displayscore():
 
     print("HIGH SCORE")
     for line in score:
-        shipCounter=(line.split("\n"))
-        print(shipCounter[0])
+        attempts = (line.split("\n"))
+        print(attempts[0])
 
 def deletescore():
-    clearscore=open('score.txt','w')
+    clearscore = open('score.txt','w')
     clearscore.close
-    
-def display(): #prints column numbers
+
+#prints column numbers  
+def display():
     i = 1
     while i <= 6:
         if i == 6:
@@ -38,30 +39,35 @@ def display(): #prints column numbers
         num += 1
         i += 1
 
-def board(ship,prob): #one board, three game modes
-
+def board(ship,prob):   
     board = []
+    #main board
     row = 1
-    probability = prob #probability differs depending on game mode
-    ships = ship #record number of ships already on the board
-
+    probability = prob
+    #probability of ship appearing in a row differs depending on difficulty mode (refer to main menu)
+    ships = ship 
+    #total number of ships depending on difficulty mode
+    
     while row <= 20:
         rowBoard = []
         column = 1
         while column <= 60:
             surprise = random.choice(probability)
             if ship == 0:
-                rowBoard.append(0) #continue appending 'empty spots' after ship count met
+                rowBoard.append(0)
+                #continue appending 'empty spots' after ship count met
                 column += 1
             else:
                 if surprise >= 1 and surprise <= 4 and surprise in rowBoard: 
-                    column = column #if duplicate (a ship represented by that number already exists), skip
+                    column = column
+                    #if duplicate (a ship represented by that number already exists), skip
                 else:
                     if surprise == 0:
                         rowBoard.append(surprise)
                         column += 1
                     else:
-                        if surprise >= 1 and surprise <= 4: #ship represented by numbers 1 to 4
+                        if surprise >= 1 and surprise <= 4:
+                            #ship represented by numbers 1 to 4
                             for i in range(5):
                                 rowBoard.append(surprise)
                         column += 5
@@ -70,8 +76,7 @@ def board(ship,prob): #one board, three game modes
         board.append(rowBoard)
         row += 1
 
-####################display time########################
-    displayBoard = []
+    displayBoard = [] #display
 
     for i in range(20):
         displayBoard.append(["#"]*60)
@@ -79,7 +84,8 @@ def board(ship,prob): #one board, three game modes
     booms = 1
     shipCounter = 0 
     while booms <= 15: 
-        if shipCounter == 5: #exit loop once 5 ships bombed
+        if shipCounter == 5:
+            #exit loop once 5 ships bombed
             display()
             countRow = 1
             for row in displayBoard:
@@ -95,7 +101,7 @@ def board(ship,prob): #one board, three game modes
                 print("".join(row), countRow)
                 countRow += 1
             try:
-                userRow, userCol = map(int, input("Enter row and col: ").split())              
+                userRow, userCol = map(int, input("Enter location to boom (row,col): ").split())              
                 if userRow < 1 or userRow > 20 or userCol < 1 or userCol > 60:
                     print ("Sorry, this ocean isn't that big.")
                     continue
@@ -116,7 +122,8 @@ def board(ship,prob): #one board, three game modes
                                 shipCounter += 1
                                 print ("You've sunk my battleship!")
                                 shipIndex = []
-                                for n, i in enumerate(board[userRow-1]): #record unmasked ship in board, unmask in display
+                                for n, i in enumerate(board[userRow-1]):
+                                    #record unmasked ship in board, unmask in display
                                     if i == bombed:
                                         shipIndex.append(board[userRow-1].index(i))
                                         board[userRow-1][n] = 5
@@ -173,24 +180,32 @@ def board(ship,prob): #one board, three game modes
                                 board[userRow-1][userCol-1] = 6
                                 displayBoard[userRow-1][userCol-1] = " "
 
+    
+        
+
     if shipCounter < 5:
-        print ("You've no luck today, try again.")
+        display
+        countRow = 1
+        for row in displayBoard:
+            print("".join(row), countRow)
+            countRow += 1
+        print ("You've no luck today, try again.\n")
 
     if attempts >= 13 and attempts <= 15:
-        print ("You are a novice.")
+        print ("You are a novice.\n")
     elif attempts >=10 and attempts <= 12:
-        print ("Not bad.")
+        print ("Not bad.\n")
     elif attempts < 10:
-        print ("You have the talent!")
+        print ("You have the talent!\n")
 
     userscore = (attempts)
-    print("Your score is",userscore)
+    print("Your score is", userscore)
 
-    scorefile=open('score.txt','r')
-    score=scorefile.readlines()
+    scorefile = open('score.txt','r')
+    score = scorefile.readlines()
     scorefile.close
 
-    scorelist=[]
+    scorelist = []
 
     #put highscores into list
     for line in score:
@@ -198,24 +213,24 @@ def board(ship,prob): #one board, three game modes
 
     #change score to integer
     for item in scorelist:
-        item[0]=int(item[0])
+        item[0] = int(item[0])
         
     #To check replace highscore or add highscore
-    if len(scorelist)==10 :
-        if userscore<(scorelist[-1][0]):
+    if len(scorelist) == 10 :
+        if userscore < (scorelist[-1][0]):
             print("Congratulations! You beat the highscore! :)")
-            username=(input("Please enter your name: ")+"\n")
-            scorelist[-1]=[userscore,username]
+            username = (input("Please enter your name: ")+"\n")
+            scorelist[-1] = [userscore,username]
             list.sort(scorelist)
         else:
             print("Try harder next time. :(")
                    
     else:
-        username=(input("Please enter your name: ")+"\n")
+        username = (input("Please enter your name: ")+"\n")
         scorelist.append([userscore,username])
         list.sort(scorelist)
     
-    writescore=open('score.txt','w')
+    writescore = open('score.txt','w')
     for item in scorelist:
         writescore.write(str(item[0])+" "+item[1])
     writescore.close()
@@ -226,21 +241,21 @@ def mainmenu():
     loop = True
     while loop:
         print("Press a to start the game \nPress b to display the highscore \nPress c to delete the highscore \nPress d to exit the game")
-        ans=input()
-        if ans =="a":
+        ans = input()
+        if ans == "a":
             try:
                 difficulty = int(input("What difficulty would you like to play? \n(1=Beginner/2=Intermediate/3=Advance)\n"))
                 if difficulty == 1:
                     board(80, [0,1,0,2,0,3,0,4,0])
-                    continue
+                    exit()
                 elif difficulty == 2:
                     board(50,[0,0,0,0,1,0,0,0,0,2,0,0,0,0,0,3,0,0,0])
-                    continue
+                    exit()
                 elif difficulty == 3:
                     board(20,[0,0,0,1,0,0,0,0,0,0,0,0])
-                    continue
+                    exit()
             except:
-                print("Please enter 1,2 or 3")
+                print("Again!")
                 continue
         elif ans == "b":
             displayscore()
